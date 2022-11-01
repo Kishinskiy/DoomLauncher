0844b2445b336da51e827119970a20dec0c41c02
= <=>
2022-11-01 03:00:59 +0300

    change structure

---
+++ src/main/java/com/kishinskii/doomlauncher/DoomProperties.java
@@ -0,0 +1,9 @@
+package com.kishinskii.doomlauncher;
+import java.util.Properties;
+
+public class DoomProperties{
+
+    private static final String userHome = System.getProperty("user.home");
+    public static String doomRoot = userHome + "/Doom";
+
+}
--- src/main/java/org/example/MapProperties.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class MapProperties extends DoomProperties{
-    public String getDoomMap() {
-        return this.getMapsFolder() + "/deserted_dawn.wad";
-    }
-}
